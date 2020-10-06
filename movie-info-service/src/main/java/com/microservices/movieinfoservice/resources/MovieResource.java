480388e2cc0116bacfe6ae6ec11d187bbc31bdfb
MrWalshyType2 <mwalsh@academytrainee.com>
2020-10-06 17:13:29 +0100

    Autowired RestTemplate

--- movie-info-service/src/main/java/com/microservices/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/microservices/movieinfoservice/resources/MovieResource.java
@@ -1,13 +1,25 @@
 package com.microservices.movieinfoservice.resources;
 
 import com.microservices.movieinfoservice.models.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+
+    /**
+     * Gets the MovieDB API key from app.properties
+     */
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{movieID}")
     public Movie getMovieInfo(@PathVariable("movieID") String movieID) {
